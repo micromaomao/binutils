44784c1e2225555a0c076a51098487d1d3686a29
Mao Wtm <m@maowtm.org>
2020-05-23 22:22:23 +0100

    Make clippy happy

--- src/bin/bytes2hex.rs
+++ src/bin/bytes2hex.rs
@@ -1,18 +1,18 @@
-use std::io::{BufReader, BufWriter, stdin, stdout, Read, Write};
+use std::io::{BufReader, BufWriter, Read, stdin, stdout, Write};
 
-use clap::{App, Arg};
+use clap::App;
 
 fn main() -> Result<(), &'static str> {
-  let args = App::new("bytes2hex")
+  let _args = App::new("bytes2hex")
       .about("Quickly convert from binary to hex.")
       .long_about("Quickly convert from binary to hex.\nRead from stdin, output to stdout.")
       .get_matches();
 
   let reader = BufReader::new(stdin());
-  let mut input_bytes = reader.bytes();
+  let  input_bytes = reader.bytes();
   let mut writer = BufWriter::new(stdout());
 
-  while let Some(byte) = input_bytes.next() {
+  for byte in input_bytes {
     let byte = byte.map_err(|_| "Failed to read.")?;
     write!(&mut writer, "{:02x}", byte).map_err(|_| "Failed to write.")?;
   }
